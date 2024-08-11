8e538a494d9a554efb882b42c2b0b91d9e036b70
jeimissonb <jeimissonb@hotmail.com>
2024-08-10 22:54:06 -0300

    Fixing app.js to resolve start problems, updating readme.md, updating collection with new routes, creating new routes to User GET, PUT and DELETE (separating in a new controller file and route file), fixing relashiopnship between task and user and add a seed insert a User in database.

--- izi-app-todo-backend/app.js
+++ izi-app-todo-backend/app.js
@@ -3,16 +3,16 @@ const express = require('express');
 const app = express();
 const authRoutes = require('./routes/authRoutes');
 const taskRoutes = require('./routes/taskRoutes');
+const userRoutes = require('./routes/userRoutes');
 const sequelize = require('./models').sequelize;
 
 app.use(express.json());
 
 app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
 app.use('/tasks', taskRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-sequelize.sync();
--- izi-app-todo-backend/models/index.js
+++ izi-app-todo-backend/models/index.js
@@ -1,17 +1,30 @@
+const fs = require('fs');
+const path = require('path');
 const Sequelize = require('sequelize');
-const config = require('../config/config.js')['development'];
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.js')[env];
+const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-const db = {};
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
 
-db.Sequelize = Sequelize;
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
 db.sequelize = sequelize;
-
-db.User = require('./user')(sequelize, Sequelize);
-db.Task = require('./task')(sequelize, Sequelize);
-
-db.User.hasMany(db.Task, { as: 'tasks' });
-db.Task.belongsTo(db.User, { foreignKey: 'userId', as: 'user' });
+db.Sequelize = Sequelize;
 
 module.exports = db;
